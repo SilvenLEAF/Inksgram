19d6b7c0ad93c22ee808f2c5aa7388df257cb46f
KADASH-KODER <67800505+KADASH-KODER@users.noreply.github.com>
2020-09-14 13:16:43 +0100

    done with the ui except one more page...will be done soon... function start now...

---
+++ client/src/AllSignInPages.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import SignInPage from './components/SignInPage';
+import LoginPage from './components/LoginPage'
+function AllSignInPages() {
+
+  return (
+    <div>  
+      <SignInPage />
+      <LoginPage />
+    </div>
+  );
+}
+
+export default  AllSignInPages;
---
+++ client/src/components/UsersMessageProfile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function UsersMessageList(){
+    return(
+        <div className ='myUsersMessagelistContainer'>
+     <span className='myUserProfileImage'>
+         <image src='' height='100%' width='100%'/>
+     </span>
+
+     <span className='myUsersMessageDetails'>
+         <h1>usersame</h1>
+         <p>message...</p>
+     </span>
+    <span className='myUserTime'>TIME</span>
+        </div>
+    )
+}
+
+export default UsersMessageList
